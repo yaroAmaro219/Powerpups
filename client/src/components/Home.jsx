6931df417cdd3b284c963cce96c0bb1f943ca730
Edward Schwartz <edwardcschwartz@gmail.com>
2020-05-20 15:47:38 -0400

    test backend routing

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-import GeoLocation from './GeoLocation'
+// import GeoLocation from './GeoLocation'
 
 export default class Home extends Component {
   render() {
     return (
       <div>
-        <GeoLocation />
+        {/* <GeoLocation /> */}
       </div>
     )
   }
