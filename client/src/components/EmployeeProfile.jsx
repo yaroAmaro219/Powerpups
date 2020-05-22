21134d615c82638ac348787f5745e631db3e7eb9
Edward Schwartz <edwardcschwartz@gmail.com>
2020-05-22 14:06:59 -0400

    finish profile modal and dashboard styling

--- client/src/components/EmployeeProfile.jsx
+++ client/src/components/EmployeeProfile.jsx
@@ -69,7 +69,6 @@
 //     </div>
 //   );
 // }
-
 import React from 'react';
 import clsx from 'clsx';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
@@ -164,13 +163,66 @@ export default function PersistentDrawerRight() {
 
   return (
     <div className={classes.root}>
-
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
+        <Toolbar>
+          <Typography variant="h6" noWrap className={classes.title}>
+            Persistent drawer
+          </Typography>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="end"
+            onClick={handleDrawerOpen}
+            className={clsx(open && classes.hide)}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <main
+        className={clsx(classes.content, {
+          [classes.contentShift]: open,
+        })}
+      >
+        <div className={classes.drawerHeader} />
+        <Typography paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
+          facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+          gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
+          donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
+          Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
+          imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
+          arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+          donec massa sapien faucibus et molestie ac.
+        </Typography>
+        <Typography paragraph>
+          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
+          facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
+          tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
+          consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
+          vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
+          hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
+          tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
+          nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
+          accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
+        </Typography>
+      </main>
       <Drawer
-        className="employee"
+        className={classes.drawer}
         variant="persistent"
         anchor="right"
-        open={true}
-
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
       >
         <div className={classes.drawerHeader}>
           <IconButton onClick={handleDrawerClose}>
@@ -178,6 +230,23 @@ export default function PersistentDrawerRight() {
           </IconButton>
         </div>
         <Divider />
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
       </Drawer>
     </div>
   );
--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -4,21 +4,49 @@ import Select, { components } from "react-select";
 
 import '../styles/SearchBar.css';
 import SearchIcon from '@material-ui/icons/Search'
-
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Slide from '@material-ui/core/Slide';
+import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import CallIcon from '@material-ui/icons/Call';
+import EmailIcon from '@material-ui/icons/Email';
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
 
 const indicatorSeparatorStyle = {
     display: 'none',
   };
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 
 class SearchBar extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      open: false,
       searchInput: "",
       selectedOption: null,
       searchClicked: false,
     };
+  }
+
+  handleProfileOpen = () => {
+    this.setState({
+      open: true 
+    });
+  }
+
+  handleProfileClose = () => {
+    this.setState({
+      open: false
+    });
   }
 
 
@@ -34,8 +62,9 @@ class SearchBar extends Component {
       <components.Option {...props}>
         <Link
           className="user-profile-link"
+          onClick={this.handleProfileOpen}
           to={{
-            pathname: "employee-profile",
+            pathname: "/",
             state: { userId: props.data.value },
           }}
         >
@@ -85,10 +114,7 @@ class SearchBar extends Component {
     return (
       <div className="search-page-container">
         <div className="search-container">
-          <form
-            className="form-container"
-            onChange={this.props.onSearchChange}
-          >
+          <form className="form-container" onChange={this.props.onSearchChange}>
             <Select
               className="react-select-container"
               value={selectedOption}
@@ -102,7 +128,7 @@ class SearchBar extends Component {
               closeMenuOnSelect={true}
               classNamePrefix="react-select"
               backspaceRemovesValue={true}
-            //   menuIsOpen={true}
+              //   menuIsOpen={true}
               styles={{
                 valueContainer: (base) => ({
                   ...base,
@@ -114,6 +140,59 @@ class SearchBar extends Component {
             />
           </form>
         </div>
+        <Dialog
+          className="profile-modal-container"
+          open={this.state.open}
+          TransitionComponent={Transition}
+          keepMounted
+          onClose={this.handleProfileClose}
+          aria-labelledby="alert-dialog-slide-title"
+          aria-describedby="alert-dialog-slide-description"
+        >
+          {/* <DialogTitle className="profile-header" id="alert-dialog-slide-title">{"Clint Osterholz"}</DialogTitle> */}
+          <DialogContent className="profile-content">
+            <img
+              className="profile-image"
+              src={require("/Users/carrielipper/Documents/General_Assembly/SEI/post_sei/datadog-hackathon-app/Powerpups/client/src/images/screencapture-keep-google-u-0-2020-05-22-12_42_46.png")}
+              alt="profile-picture"
+            ></img>
+            <div className="profile-introduction">
+              <i className="away-status"><FiberManualRecordIcon /></i>
+              <span className="profile-name">Clint</span>
+              <span className="profile-last-name">Osterholz</span>
+              <span className="profile-pronouns">(He/Him)</span>
+            </div>
+
+            <div className="employee-title">Sales Engineer</div>
+
+            <div className="profile-subcontainer">
+              <i className="profile-icon"><LocationOnIcon /></i>
+              <div className="profile-data">New York, NY</div>
+            </div>
+            <div className="profile-subcontainer">
+              <i className="profile-icon"><CallIcon /></i>
+              <div className="profile-data">917-234-5678</div>
+            </div>
+            <div className="profile-subcontainer">
+              <i className="profile-icon"><EmailIcon /></i>
+              <div className="profile-data">clint@fakemail.com</div>
+            </div>
+            <div className="profile-subcontainer">
+              <i className="profile-icon"><AccessTimeIcon /></i>
+              <div className="profile-data">9:57 p.m.</div>
+            </div>
+            
+
+          </DialogContent>
+          <DialogActions className="profile-footer">
+            {/* <Button onClick={this.handleProfileClose} color="primary">
+              Message
+            </Button>
+            <Button onClick={this.handleProfileClose} color="primary">
+              Add to Team
+            </Button> */}
+          </DialogActions>
+        </Dialog>
       </div>
     );
   }
